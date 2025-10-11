34207b7f07fac1aa458df66cb415fd5ae738bf66
sky0621 <sky14work@gmail.com>
2025-10-11 23:53:55 +0900

    -

--- services/internal/domain/health.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "time"
-
-// HealthStatus represents the status of the application.
-type HealthStatus struct {
-	Status    string    `json:"status"`
-	CheckedAt time.Time `json:"checked_at"`
-}
